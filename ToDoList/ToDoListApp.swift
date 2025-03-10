a203e1c200cd342d79ec6c74879f90fd52b363b7
Chigozie Sumani <sumanic@bc.edu>
2025-03-10 14:05:18 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Chigozie Sumani on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
